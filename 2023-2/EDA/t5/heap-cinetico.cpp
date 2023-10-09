3918bea7fb9853124e71b777cf4222aa2a3c571e
Paulo Henrique <paulo13579691@gmail.com>
2023-10-09 01:30:34 -0300

    versão sem Delete()

--- 2023-2/EDA/t5/heap-cinetico.cpp
+++ 2023-2/EDA/t5/heap-cinetico.cpp
@@ -5,25 +5,79 @@
 #include "heap.cpp"
 #include <map>
 
-#define INFTY 123456
+
+#define DEBUG_MODE_2 1
+#if DEBUG_MODE_2
+#define DEBUG2(x) std::cout << x
+#else
+#define DEBUG2(x) 
+#endif
 
 class KinHeap {
   public:
-    KinHeap(std::vector<int> id, std::vector<int> x0, std::vector<int> speed, int n)
-    {
-      H = std::vector<int>(); H.push_back(-1);
-      Q = new Heap();
 
-      BuildKinHeap(id, x0, speed, n);
-      BuildCertificates();
+    // Constrói KinHeap vazio
+    KinHeap()
+    {
+      now = 0;
+      m = std::map<int, std::tuple<double, double, int>>();
+      H = std::vector<int>(); H.push_back(-1); // Não utilizo primeira posição
+      Q = new Heap();
+    }
+
+    // Constrói KinHeap a partir de valores de entrada
+    KinHeap(int id[], double x0[], double v[], int n)
+    {
+      now = 0;
+      m = std::map<int, std::tuple<double, double, int>>();
+      BuildHeap(id, x0, v, n);
+      BuildQ();
+    }
+
+    // Insere o elemento id, cujo valor atual é xnow com velocidade v
+    void Insert(int id, double xnow, double v)
+    {
+      // Adiciona elemento id no MaxHeap H
+      H.push_back(id);
+      int i = H.size() - 1;
+      int new_x0 = xnow - v * now; 
+      m[id] = std::make_tuple(new_x0, v, i);
+
+      if(i == 1) return; // Raiz
+
+      int parent_id = H[i/2];
+      if(Key(id, now) > Key(parent_id, now)) Q->Insert(i, now);
+      else Q->Insert(i, Validade(i));
+
+      while(Q->Min().second == now)
+      {
+        Event(Q->Min().first, Q->Min().second);
+        
+        int i = std::get<2>(m[id]);
+
+        if(i / 2 == 0) break;
+
+        int parent_id = H[i/2];
+        if(Key(id, now) > Key(parent_id, now)) Q->Update(i, now);
+        else Q->Update(i, Validade(i));
+      }
+
     }
 
     void Advance(const int t)
     {
+      int num_it = 0;
       while(Q->Min().second <= t)
       {
         now = Q->Min().second;
-        Event(Q->Min());
+        DEBUG2("now = "); DEBUG2(now); DEBUG2("\n");
+        Event(Q->Min().first, Q->Min().second);
+        DEBUG2("Depois do evento:\n");
+        Print();
+        Q->Print();
+
+        num_it++;
+        //if(num_it == 1) break;
       }
       now = t;
     }
@@ -42,35 +96,46 @@ class KinHeap {
       if(2*i + 1 < H.size()) UpdateCert(2*i + 1);
     }
 
-    void Insert(const int id, const int xnow, const int v)
-    {
+    // Retorna id do elemento com maior valor
+    int Max() { return H[1]; }
 
-    }
-
-    int Max()
-    {
-
-    }
-
-    void DeleteMax()
-    {
-
-    }
-
-    void DeleteId(const int id)
-    {
-
-    }
-
+     
+    // Imprime representação de árvore do heap, junto com a representação de vetor
     void Print()
     {
+      //std::cout << "Representação de árvore: \n";
+      //_PrintTree(0, 1); 
 
+      std::cout << "H: \n";
+      for(int i = 1; i <= H.size() - 1; i++) std::cout << H[i] << " ";
+      std::cout << "\n";
+
+      //std::cout << "Mapeamento: \n";
+      //for(int i = 2; i <= map.size() - 1; i++) std::cout << "map[" << i << "] = " << map[i] << "\n";
     }
 
-  private:
-    
-    // Atualiza o certificado i
-    void UpdateCert(int i)
+     //private:
+
+    void _PrintTree(int s, int i)
+    {
+      if(i > H.size() - 1) return;
+
+      _PrintTree(s + 3, 2*i);
+
+      std::string space (s, ' ');
+      std::cout << space;
+
+      int id = H[i];
+      double xnow = Key(id, now);
+      double x0 = std::get<0>(m[id]);
+      double v = std::get<1>(m[id]);
+      std::cout << xnow << " (" << x0 << " " << v << ")\n";
+      _PrintTree(s + 3, 2*i + 1);
+    }
+
+
+    // Retorna prazo de validade para certificado i
+    double Validade(int i)
     {
       int u = H[i];
       int u_parent = H[i/2];
@@ -80,16 +145,79 @@ class KinHeap {
       double x_u_parent = std::get<0>(m[u_parent]);
       double v_u_parent = std::get<1>(m[u_parent]);
 
+      if(v_u == v_u_parent) return INFTY;
+      //DEBUG2(v_u_parent); DEBUG2("\n");
+      //DEBUG2(v_u); DEBUG2("\n");
+      //DEBUG2(x_u-x_u_parent); DEBUG2("\n");
+      //DEBUG2(v_u_parent - v_u); DEBUG2("\n");
+      double t = (x_u - x_u_parent) / (v_u_parent - v_u);
+      //DEBUG2("t = "); DEBUG2(t); DEBUG2("\n");
+      if(t < 0) return INFTY;
+      else return t;
+    }
+    
+    // Constrói heap cinético a partir da entrada 
+    void BuildHeap(int _id[], double _x0[], double _v[], int n)
+    {
+      H.push_back(-1); // Não utilizo primeira posição
+    
+      // Copia para H
+      for(int i = 0; i < n; i++)
+      {
+        int id = _id[i];
+        double x0 = _x0[i];
+        double v = _v[i];
+
+        H.push_back(id);
+        std::get<0>(m[id]) = x0;
+        std::get<1>(m[id]) = v;
+        std::get<2>(m[id]) = H.size() - 1;
+      }
+      
+      // Restaura propriedade de heap
+      for(int i = H.size()/2; i > 0; i--) Sink(i);
+    }
+
+    // Constrói MinHeap Q de certificados
+    void BuildQ()
+    {
+      Q = new Heap();
+      int num_cert = H.size() - 2;
+      for(int cert_id = 2; cert_id <= 2 + num_cert - 1; cert_id++) // num_cert = num_elementos - 1 = H.size() - 1 - 1 = H.size() - 2
+      {
+        Q->Insert(cert_id, Validade(cert_id));
+      }
+    }
+
+    // Atualiza o certificado i
+    void UpdateCert(int i)
+    {
+      DEBUG2("Update("); DEBUG2(i); DEBUG2(")\n");
+      if(i == 1) return;
+      int u = H[i];
+      int u_parent = H[i/2];
+
+      double x_u = std::get<0>(m[u]);
+      double v_u = std::get<1>(m[u]);
+      double x_u_parent = std::get<0>(m[u_parent]);
+      double v_u_parent = std::get<1>(m[u_parent]);
+      
       double t;
       if(v_u == v_u_parent) t = INFTY;
-      else t = (x_u - x_u_parent) / (v_u_parent - v_u);
-
+      t = (x_u - x_u_parent) / (v_u_parent - v_u);
+      if(t <= now) t = INFTY;
+      DEBUG2("t = "); DEBUG2(t); DEBUG2("\n");
       Q->Update(i, t);
     }
 
     // Certificado (i, t) expirou
-    void Event(int i, int t)
+    void Event(int i, double t)
     {
+      DEBUG2("Event("); DEBUG2(i); DEBUG2(", "); DEBUG2(t); DEBUG2(")\n");
+      int id = H[i];
+      int parent_id = H[i/2];
+      DEBUG2("id = "); DEBUG2(id); DEBUG2(", parent_id = "); DEBUG2(parent_id); DEBUG2("\n");
+
       Swap(i, i/2);
       
       if(i > 1) UpdateCert(i/2);
@@ -97,6 +225,7 @@ class KinHeap {
       if(2*i < H.size()) UpdateCert(2*i);
       if(2*i + 1 < H.size()) UpdateCert(2*i + 1);
       int s = Sibling(i);
+      DEBUG2("sibling = "); DEBUG2(s); DEBUG2("\n");
       if(s > 0) UpdateCert(s);
     }
 
@@ -118,24 +247,24 @@ class KinHeap {
     }
 
     // Sobe elemento na posição i do vetor H para a sua posição correta no heap
-    void Swim(int i)
-    {
-      if(i/2 < 1) return;
+    //void Swim(int i)
+    //{
+    //  if(i/2 < 1) return;
 
-      int u_id = H[i];
-      int u_parent_id = H[i/2];
+    //  int u_id = H[i];
+    //  int u_parent_id = H[i/2];
 
-      int u_key = Key(u_id, now);
-      int u_parent_key = Key(u_parent_id, now);
-      
-      if(u_key > u_parent_key) 
-      { 
-        Swap(i, i/2); 
-        Swim(i/2);
-      }
-    }
+    //  int u_key = Key(u_id, now);
+    //  int u_parent_key = Key(u_parent_id, now);
+    //  
+    //  if(u_key > u_parent_key) 
+    //  { 
+    //    Swap(i, i/2); 
+    //    Swim(i/2);
+    //  }
+    //}
 
-    // Dado um id, calcula a chave no tempo t
+    // Dado um id de um elemento, calcula a chave no tempo t
     int Key(int id, int t)
     {
       int x0 = std::get<0>(m[id]);
@@ -144,12 +273,15 @@ class KinHeap {
     }
 
     
-    // Troca a posição dos elemetos nas posições i e j
+    // Troca de posição os elemetos nos índices i e j em H
     void Swap(const int i, const int j)
     {
-      int tmp = H[i];
-      H[i] = H[j];
-      H[j] = tmp;
+      // Atualiza mapeamento de índice
+      int i_id = H[i]; std::get<2>(m[i_id]) = j;
+      int j_id = H[j]; std::get<2>(m[j_id]) = i;
+
+      H[i] = j_id;
+      H[j] = i_id;
     }
 
     // Desce o elemento na posição i do vetor H para a sua posição correta no heap cinético
@@ -187,11 +319,153 @@ class KinHeap {
       }
     }
    
-    std::vector<int> H; // Vetor que representa o heap
+    std::vector<int> H; // Vetor que representa o heap. Guarda os ids dos elementos
     Heap* Q; // MinHeap de certificados
-    std::map<int, std::tuple<double, double, int>> m; // Mapeia ids para posição inicial e velocidade e posição no vetor H
-    int now; // Instante atual
-
+    std::map<int, std::tuple<double, double, int>> m; // Mapeia ids dos elementos para posição inicial, velocidade e posição no vetor H
+    double now; // Instante atual
 };
 
+// Teste inicial para construção do KinHeap vazio e inserção de alguns elementos. Parece OK
+void Teste1()
+{
+  KinHeap kh = KinHeap();
+  kh.Insert(1, 17, -1);
+  kh.Insert(2, 10, 1);
+  kh.Insert(3, 3, 5);
+  kh.Insert(4, 0, 2.1);
+  kh.Insert(5, 3, 0);
+
+  std::cout << "******************************\n";
+  std::cout << "Inicialmente:\n";
+  kh.Print(); 
+  std::cout << "certificados: \n";
+  kh.Q->Print();
+  //std::cout << "(" << kh.Q->Min().first << ", " << kh.Q->Min().second << ")\n";
+  std::cout << "Max = " << kh.Max() << "\n";
+
+  int t = 10;
+  std::cout << "******************************\n";
+  std::cout << "avançando para t = " << t << ":\n";
+  kh.Advance(t);
+
+  kh.Print(); 
+  std::cout << "certificados: \n";
+  kh.Q->Print();
+  std::cout << "Max = " << kh.Max() << "\n";
+}
+
+// Testa construir Heap a partir dos elementos iniciais. Parece OK
+void Teste2()
+{
+  int id[] = {2, 5, 1, 3, 4};
+  double x0[] = {2.2, -7.4, 0, 1.1, 12.0};
+  double v[] = {-1.2, 1.1, 5.5, 2.1, -0.1};
+  int n = (int) (sizeof(id)/sizeof(id[0]));
+  KinHeap kh = KinHeap(id, x0, v, n);
+
+  kh.Print();
+  std::cout << "certificados: \n";
+  kh.Q->Print();
+  std::cout << "Max = " << kh.Max() << "\n";
+
+  std::cout << "***********************\n";
+  int t = 3;
+  std::cout << "Avançando até t = " << t << "\n";
+  kh.Advance(t);
+  kh.Print();
+  std::cout << "certificados: \n";
+  kh.Q->Print();
+  std::cout << "Max = " << kh.Max() << "\n";
+}
+
+// Cliente principal
+void Teste()
+{
+  int n; std::cin >> n;
+  int id[n];
+  double x0[n];
+  double v[n];
+  for(int i = 0; i < n; i++) {
+    std::cin >> x0[i] >> v[i];
+    id[i] = i + 1;
+  }
+
+  KinHeap kh = KinHeap(id, x0, v, n);
+
+  for(std::string line; std::getline(std::cin, line) ; ) {
+    int opt, a, b, c; 
+    std::istringstream iss = std::istringstream(line);
+    iss >> opt >> a >> b >> c;
+    switch (opt) {
+      case 1:
+        kh.Advance(a);
+        break;
+      case 2:
+        kh.Change(a, b);
+        break;
+      case 3:
+        kh.Insert(a, b, c);
+        break;
+      case 4:
+        std::cout << kh.Max() << "\n";
+        break;
+      case 5:
+        //kh.DeleteMax();
+        break;
+      case 6:
+        //kh.Delete(a);
+        break;
+      case 7:
+        kh.Print();
+        break;
+    }
+  }
+}
+
+// Loop infinito na entrada de teste 
+void Teste3()
+{
+  int id[] = {1, 2, 3, 4, 5};
+  double x0[] = {8, 3, 17, 16, 5};
+  double v[] = {5, 4, -1, 1, 0};
+  int n = (int) (sizeof(id)/sizeof(id[0]));
+  KinHeap kh = KinHeap(id, x0, v, n);
+
+  //kh.Print();
+  //kh.Q->Print();
+  kh.Advance(4);
+  kh.Print();
+}
+
+// Desempate. Os três elementos colidem em t = 1.5. Não consegui quebrar ainda
+void Teste4()
+{
+  int id[] = {1, 2, 3};
+  double x0[] = {8, 17, -2.65};
+  double v[] = {5, -1, 12.1};
+  int n = (int) (sizeof(id)/sizeof(id[0]));
+  KinHeap kh = KinHeap(id, x0, v, n);
+
+  kh.Print();
+  kh.Q->Print();
+  std::cout << "Max = " << kh.Max() << "\n";
+
+  kh.Advance(4);
+  kh.Print();
+  std::cout << "Max = " << kh.Max() << "\n";
+}
+
+int main()
+{
+  //Teste1();
+  //Teste2();
+  //Teste3();
+  Teste4();
+  //Teste();
+}
+
+//int main(int argc, char** argv) {
+//    ::testing::InitGoogleTest(&argc, argv);
+//    return RUN_ALL_TESTS();
+//}
 
--- 2023-2/EDA/t5/heap.cpp
+++ 2023-2/EDA/t5/heap.cpp
@@ -1,22 +1,23 @@
-// Tarefa t5 - Heap normal que guarda certificados
+// Tarefa t5 - Heap de certificados
 // Paulo Henrique Albuquerque
 // 12542251
 
 #include <iostream>
 #include <vector>
 #include <string>
+#include <gtest/gtest.h>
 
 #define GARBAGE std::make_pair(-1,-1)
+#define INFTY 123456
 
 #define DEBUG_MODE 0
-
 #if DEBUG_MODE
 #define DEBUG(x) std::cout << x
 #else
 #define DEBUG(x) 
 #endif
 
-typedef std::pair<int, int> cert;
+typedef std::pair<int, double> cert;
 
 class Heap {
   
@@ -26,15 +27,32 @@ class Heap {
     Heap()
     {
       h = std::vector<cert>(); h.push_back(GARBAGE); // Não usamos a primeira posição
-      map = std::vector<int>(); map.push_back(-1); map.push_back(-1); // Naõ usamos a duas primeiras posições, pois os ids dos certificados começam de 2
+      map = std::vector<int>(); map.push_back(-1); map.push_back(-1); // Não usamos a duas primeiras posições, pois os ids dos certificados começam de 2
     }
 
-    // Insere cerficado (id, t) no heap
-    void Insert(const int id, const int t)
+    // Constrói heap a partir do vetor de certificados v
+    Heap(std::vector<cert> v)
+    {
+      h = std::vector<cert>(); h.push_back(GARBAGE);
+      map = std::vector<int>(); map.push_back(-1); map.push_back(-1);
+
+      for(int i = 0; i < v.size(); i++) {
+        h.push_back(v[i]);
+        int current_heap_size = h.size() - 1;
+        int id = v[i].first;
+        if(id > current_heap_size) map.push_back(1); // Só adiciona mais uma posição à map[] se id não foi adicionado anteriormente
+        map[id] = current_heap_size;
+      }
+
+      for(int i = h.size()/2; i > 0; i--) Sink(i);
+    }
+
+    // Insere cerficado (id, t) no heap. Pressupõe que os ids dos certificados são 2, 3, ..., 2 + num_certificados - 1
+    void Insert(const int id, const double t)
     {
       h.push_back(std::make_pair(id, t));
       int current_heap_size = h.size() - 1;
-      map.push_back(1);
+      if(id > current_heap_size) map.push_back(1); // Só adiciona mais uma posição à map[] se id não foi adicionado anteriormente
       map[id] = current_heap_size;
       Swim(current_heap_size);
     }
@@ -50,10 +68,10 @@ class Heap {
     }
 
     // Retorna certificado (id, t) com menor t 
-    std::pair<int, int> Min() { return h[1]; }
+    cert Min() { return h[1]; }
 
     // Atualiza prioridade do certificado id para new_t
-    void Update(const int id, const int new_t)
+    void Update(const int id, const double new_t)
     {
       h[map[id]].second = new_t;
       Sink(map[id]);
@@ -63,13 +81,14 @@ class Heap {
     // Imprime representação de árvore do heap, junto com a representação de vetor
     void Print()
     {
-      std::cout << "Representação de árvore: \n";
-      _PrintTree(0, 1); 
-      std::cout << "Representação de vetor: \n";
-      for(int i = 1; i <= h.size() - 1; i++) std::cout << "(" << h[i].first << "," << h[i].second << "), ";
+      //std::cout << "Representação de árvore: \n";
+      //_PrintTree(0, 1); 
+      //std::cout << "Representação de vetor: \n";
+      for(int i = 1; i <= h.size() - 1; i++) std::cout << "(" << h[i].first << "," << (h[i].second == INFTY ? "inf" : std::to_string(h[i].second)) << "), ";
       std::cout << "\n";
-      std::cout << "Mapeamento: \n";
-      for(int i = 2; i <= map.size() - 1; i++) std::cout << "map[" << i << "] = " << map[i] << "\n";
+
+      //std::cout << "Mapeamento: \n";
+      //for(int i = 2; i <= map.size() - 1; i++) std::cout << "map[" << i << "] = " << map[i] << "\n";
     }
 
 
@@ -157,105 +176,136 @@ class Heap {
       }
     }
     
-    std::vector<std::pair<int, int>> h; // vetor que representa árvore do heap
+    std::vector<cert> h; // vetor que representa árvore do heap
     std::vector<int> map; // mapeamento id -> índice em h
 
 };
 
-void Teste1()
-{
-  Heap h = Heap();
-  int insertion_list[] = {27, 17, 18, 19, 11, 5, 9, 33, 14, 21};
-  int size = (int) (sizeof(insertion_list) / sizeof(insertion_list[0]));
+//TEST(Heap, Teste1)
+//{
+//  Heap h = Heap();
+//  int insertion_list[] = {27, 17, 18, 19, 11, 5, 9, 33, 14, 21};
+//  int size = (int) (sizeof(insertion_list) / sizeof(insertion_list[0]));
+//
+//  for(int i = 0; i < size; i++){
+//    h.Insert(i + 2, insertion_list[i]);
+//  }
+//
+//  h.Print();
+//  cert min = h.Min();
+//  //std::cout << "(" <<  min.first << "," << min.second << ")\n";
+//
+//  std::cout << "Deletando mínimo\n";
+//  h.DeleteMin();
+//  h.Print();
+//  min = h.Min();
+//  std::cout << "(" <<  min.first << "," << min.second << ")\n";
+//
+//  std::cout << "Deletando mínimo\n";
+//  h.DeleteMin();
+//  h.Print();
+//  min = h.Min();
+//  std::cout << "(" <<  min.first << "," << min.second << ")\n";
+//
+//  std::cout << "Deletando mínimo\n";
+//  h.DeleteMin();
+//  h.Print();
+//  min = h.Min();
+//  std::cout << "(" <<  min.first << "," << min.second << ")\n";
+//}
+//
+//TEST (Heap, Teste2)
+//{
+//  Heap h = Heap();
+//  int insertion_list[] = {27, 17, 18, 19, 11, 5, 9, 33, 14, 21};
+//  int size = (int) (sizeof(insertion_list) / sizeof(insertion_list[0]));
+//
+//  for(int i = 0; i < size; i++){
+//    h.Insert(i + 2, insertion_list[i]);
+//  }
+//
+//  h.Print();
+//}
+//
+//TEST(Heap, Teste3)
+//{
+//  Heap h = Heap();
+//  int insertion_list[] = {27, 17, 18, 19, 11, 5, 9, 33, 14, 21};
+//  int size = (int) (sizeof(insertion_list) / sizeof(insertion_list[0]));
+//
+//  for(int i = 0; i < size; i++){
+//    h.Insert(i + 2, insertion_list[i]);
+//  }
+//
+//  h.Print();
+//
+//  h.Update(2, 3);
+//  h.Print();
+//
+//  h.Update(9, 12);
+//  h.Print();
+//}
+//
+//TEST (Heap, Teste4)
+//{
+//  Heap h = Heap();
+//  h.Insert(2,3);
+//  h.Insert(3,11);
+//  h.Insert(4,11);
+//
+//  h.Print();
+//  cert min = h.Min();
+//  std::cout << "(" <<  min.first << "," << min.second << ")\n";
+//
+//  h.DeleteMin();
+//  h.Print();
+//  min = h.Min();
+//  std::cout << "(" <<  min.first << "," << min.second << ")\n";
+//
+//  h.DeleteMin();
+//  h.Print();
+//  min = h.Min();
+//  std::cout << "(" <<  min.first << "," << min.second << ")\n";
+//
+//}
+//
+//// Delete + Insert pode dar ruim
+//TEST (Heap, Teste5)
+//{
+//  Heap h = Heap();
+//  h.Insert(2,3);
+//  h.Insert(3,10);
+//  h.Insert(4,11);
+//  h.Print();
+//
+//  cert min = h.Min();
+//  h.DeleteMin();
+//  h.Print();
+//
+//  h.Insert(min.first, 4);
+//  h.Print();
+//
+//}
+//
+//// Criado heap a partir de vetor
+//TEST(Heap, Teste6)
+//{
+//  std::vector<cert> v;
+//  v.push_back(std::make_pair(2,27));
+//  v.push_back(std::make_pair(3,17));
+//  v.push_back(std::make_pair(4,18));
+//  v.push_back(std::make_pair(5,19));
+//  v.push_back(std::make_pair(6,11));
+//  v.push_back(std::make_pair(7,5));
+//  v.push_back(std::make_pair(8,9));
+//  v.push_back(std::make_pair(9,33));
+//  v.push_back(std::make_pair(10,14));
+//  v.push_back(std::make_pair(11,21));
+//  Heap h = Heap(v);
+//  h.Print();
+//}
 
-  for(int i = 0; i < size; i++){
-    h.Insert(i + 2, insertion_list[i]);
-  }
-
-  h.Print();
-  cert min = h.Min();
-  //std::cout << "(" <<  min.first << "," << min.second << ")\n";
-
-  std::cout << "Deletando mínimo\n";
-  h.DeleteMin();
-  h.Print();
-  min = h.Min();
-  std::cout << "(" <<  min.first << "," << min.second << ")\n";
-
-  std::cout << "Deletando mínimo\n";
-  h.DeleteMin();
-  h.Print();
-  min = h.Min();
-  std::cout << "(" <<  min.first << "," << min.second << ")\n";
-
-  std::cout << "Deletando mínimo\n";
-  h.DeleteMin();
-  h.Print();
-  min = h.Min();
-  std::cout << "(" <<  min.first << "," << min.second << ")\n";
-}
-
-void Teste2()
-{
-  Heap h = Heap();
-  int insertion_list[] = {27, 17, 18, 19, 11, 5, 9, 33, 14, 21};
-  int size = (int) (sizeof(insertion_list) / sizeof(insertion_list[0]));
-
-  for(int i = 0; i < size; i++){
-    h.Insert(i + 2, insertion_list[i]);
-  }
-
-  h.Print();
-}
-
-void Teste3()
-{
-  Heap h = Heap();
-  int insertion_list[] = {27, 17, 18, 19, 11, 5, 9, 33, 14, 21};
-  int size = (int) (sizeof(insertion_list) / sizeof(insertion_list[0]));
-
-  for(int i = 0; i < size; i++){
-    h.Insert(i + 2, insertion_list[i]);
-  }
-
-  h.Print();
-
-  h.Update(2, 3);
-  h.Print();
-
-  h.Update(9, 12);
-  h.Print();
-}
-
-void Teste4()
-{
-  Heap h = Heap();
-  h.Insert(2,3);
-  h.Insert(3,11);
-  h.Insert(4,11);
-
-  h.Print();
-  cert min = h.Min();
-  std::cout << "(" <<  min.first << "," << min.second << ")\n";
-
-  h.DeleteMin();
-  h.Print();
-  min = h.Min();
-  std::cout << "(" <<  min.first << "," << min.second << ")\n";
-
-  h.DeleteMin();
-  h.Print();
-  min = h.Min();
-  std::cout << "(" <<  min.first << "," << min.second << ")\n";
-
-
-
-}
-
-int main()
-{
-  //Teste1();
-  //Teste2();
-  //Teste3();
-  Teste4();
-}
+//int main(int argc, char** argv) {
+//    ::testing::InitGoogleTest(&argc, argv);
+//    return RUN_ALL_TESTS();
+//  }
