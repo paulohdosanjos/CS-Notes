87e9c7efb3536c2422ca30816808247b4d67a4f5
Paulo Henrique <paulodosanjos@usp.br>
2023-10-28 20:41:58 -0300

    coloca alguns valores como hardcodec

---
+++ 2023-2/Redes/ep1/hardcoded.h
@@ -0,0 +1,13 @@
+const extern unsigned char CONNECTION_START_FRAME[];
+
+const extern unsigned char CONNECTION_TUNE_FRAME[];
+
+const extern unsigned char CONNECTION_OPEN_OK_FRAME[];
+
+const extern unsigned char CONNECTION_CLOSE_OK_FRAME[];
+
+const extern unsigned char CHANNEL_OPEN_OK_FRAME[];
+
+const extern unsigned char CHANNEL_CLOSE_OK_FRAME[];
+
+const extern unsigned char DEFAULT_PROTOCOL_HEADER[];
